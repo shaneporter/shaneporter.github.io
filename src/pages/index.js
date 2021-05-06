f6afc6333bd3894ea9cac650c90f193a690bf312
Shane Porter <shane@freshclickmedia.com>
2021-05-06 22:02:34 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
 import React from "react"
 
-export default () => <div>Shane Porter is a JavaScript consultant based in Wiltshire, UK.</div>
+export default () => <div>Shane Porter JavaScript developer based in Wiltshire, UK.</div>

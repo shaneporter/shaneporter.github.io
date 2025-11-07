beafa5c394c4ac9c3ca06839318159041473e179
Shane Porter <shane@freshclickmedia.com>
2025-11-07 20:33:03 +0000

    refactor: migrate from Next.js to Astro

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-32">
-      Shane Porter is a JavaScript developer working with Google Cloud
-    </main>
-  );
-}
